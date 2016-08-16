20b4987c65ae7dfa7e76f970e580e08ab883daf3
Dylan Ng <ngxinde@gmail.com>
2016-08-17 00:52:57 +0800

    added show button for quote

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(function() {
   var $profileURL   = $('.profileURL'),
       $name         = $('.name'),
       $profession   = $('.profession'),
+      $quoteBtn     = $('.quote-btn'),
       $quote        = $('.quote');
   var $url          = 'https://radiant-lake-39661.herokuapp.com/dylan';
 
@@ -14,8 +15,15 @@ $(function() {
     $name.text(data.name);
     $profession.text(data.profession);
     $quote.text(data.quote);
+    $quote.hide();
   })
   .fail(function(req, textStatus, errorThrown) {
     console.log(errorThrown);
   });
+
+  $quoteBtn.on('click', function() {
+    $quoteBtn.hide();
+    $quote.show();
+  });
+
 });
